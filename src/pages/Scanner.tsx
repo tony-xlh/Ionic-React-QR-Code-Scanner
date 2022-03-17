5328927f50f8d11e6af1793732421602a0fece05
xulihang <xulihanghai@163.com>
2022-03-17 13:41:38 +0800

    add rotation handlement; use state correctly; fix multiple goback

--- src/pages/Scanner.tsx
+++ src/pages/Scanner.tsx
@@ -5,19 +5,19 @@ import { RouteComponentProps } from "react-router";
 import QRCodeScanner from "../components/QRCodeScanner";
 import "./Scanner.css"
 
-let continuousScan = false;
-let qrcodeOnly = true;
+
 
 const Scanner = (props:RouteComponentProps) => {
   const [initialized,setInitialized] = useState(false);
   const [cameras,setCameras] = useState([] as string[]);
   const [barcodeResults,setBarcodeResults] = useState([] as TextResult[]);
-  const [isActive,setIsActive] = useState(true);
+  const [isActive,setIsActive] = useState(false);
   const [torchOn,setTorchOn] = useState(false);
   const [cameraID,setCameraID] = useState("");
   const [frameWidth,setFrameWidth] = useState(1920);
   const [frameHeight,setFrameHeight] = useState(1080);
-  const state = props.location.state as { continuousScan: boolean; qrcodeOnly: boolean; active: boolean; result?: string};
+  const [viewBox,setViewBox] = useState("0 0 1920 1080");
+  let scanned = false;
 
   const loadCameras = async () => {
     let result = await DBR.getAllCameras();
@@ -26,7 +26,8 @@ const Scanner = (props:RouteComponentProps) => {
     }
   }
 
-  const setQRCodeRuntimeSettings = () => {
+  const setQRCodeRuntimeSettings = (qrcodeOnly:boolean) => {
+    console.log("qrcode only: "+qrcodeOnly);
     if (qrcodeOnly == true) {
       let template = "{\"ImageParameter\":{\"BarcodeFormatIds\":[\"BF_QR_CODE\"],\"Description\":\"\",\"Name\":\"Settings\"},\"Version\":\"3.0\"}";
       console.log(template);
@@ -39,28 +40,12 @@ const Scanner = (props:RouteComponentProps) => {
   }
 
   useEffect(() => {
-    console.log("state changed");
-    console.log(state);
-    
-    if (state) {
-      if (state.active != undefined) {
-        setIsActive(state.active);
-      }
-      if (state.continuousScan != undefined) {
-        continuousScan = state.continuousScan;
-      }
-      if (state.qrcodeOnly != undefined && initialized) {
-        qrcodeOnly = state.qrcodeOnly;
-        setQRCodeRuntimeSettings();
-      }
-    }
-  },[props.location.state]);
-
-  useEffect(() => {
     console.log("on mount");
-    console.log("continuousScan: "+continuousScan);
-    console.log("qrcodeOnly: "+qrcodeOnly);
-
+    const state = props.location.state as { continuousScan: boolean; qrcodeOnly: boolean; active: boolean; result?: string};
+    console.log(state);
+    if (state && state.active != true) {
+      return;
+    } 
     async function init() {
       let result = await DBR.initialize();
       console.log(result);
@@ -68,54 +53,50 @@ const Scanner = (props:RouteComponentProps) => {
         if (result.success == true) {
           setInitialized(true);
           loadCameras();
-          setQRCodeRuntimeSettings();
+          setQRCodeRuntimeSettings(state.qrcodeOnly);
           DBR.addListener('onFrameRead', async (scanResult:ScanResult) => {
             let results = scanResult["results"];
-            if (continuousScan) {
-              console.log(results);
-              if (scanResult.deviceOrientation) {
-                if (scanResult.deviceOrientation == "portrait") {
-                  if (frameWidth>frameHeight){
-                    const height = frameWidth;
-                    const width = frameHeight;
-                    setFrameHeight(height);
-                    setFrameWidth(width);
-                    alert(width+"x"+height);
-                  }
+            if (state.continuousScan) {
+              if (scanResult.frameOrientation != undefined && scanResult.deviceOrientation != undefined) {
+                for (let index = 0; index < results.length; index++) {
+                  handleRotation(results[index], scanResult.deviceOrientation, scanResult.frameOrientation);
                 }
+                updateViewBox(frameWidth,frameHeight,scanResult.deviceOrientation);
               }
               setBarcodeResults(results);
             }else{
-              if (results.length>0) {
+              if (results.length>0 && scanned == false) {
                 setIsActive(false);
+                scanned = true;
                 let result = "";
                 for (let index = 0; index < results.length; index++) {
                   const tr:TextResult = results[index];
                   result = result + tr.barcodeFormat + ": " + tr.barcodeText + "\n";
                 }
-                props.history.replace({ state: {result:result} });
+                props.history.replace({ state: {result:result,active:false} });
                 props.history.goBack();
               }
             }
           });
           DBR.addListener("onPlayed", (result:{resolution:string}) => {
-            alert("on played");
-            alert(result.resolution);
             console.log("onPlayed");
             console.log(result);
-            let resolution: string = result.resolution;
-            setFrameWidth(parseInt(resolution.split("x")[0]));
-            setFrameHeight(parseInt(resolution.split("x")[1]));
+            const resolution: string = result.resolution;
+            const width = parseInt(resolution.split("x")[0]);
+            const height = parseInt(resolution.split("x")[1]);
+            setFrameWidth(width);
+            setFrameHeight(height);
+            updateViewBox(width,height);
           });
         }
       }
     }
     init();
-    
+    scanned = false;
+    setIsActive(true);
   }, []);
   
   const onCameraSelected = (e: any) => {
-    console.log(e);
     setCameraID(e.target.value);
   }
 
@@ -132,8 +113,80 @@ const Scanner = (props:RouteComponentProps) => {
     return pointsData;
   }
 
+  const handleRotation = (result:any, orientation: string, rotation:number) => {
+    let width,height;
+    if (orientation == "portrait") {
+      width = frameHeight;
+      height = frameWidth;
+    }else{
+      width = frameWidth;
+      height = frameHeight;
+    }
+    for (let i = 1; i < 5; i++) {
+      let x = result["x"+i];
+      let y = result["y"+i];
+      let rotatedX;
+      let rotatedY;
+      
+      switch (rotation) {
+        case 0:
+          rotatedX = x;
+          rotatedY = y;
+          if (isFront() == true){ //front cam landscape
+            rotatedX = width - rotatedX;
+          }
+          break;
+        case 90:
+          rotatedX = width - y;
+          rotatedY = x;
+          if (isFront() == true){ //front cam portrait
+            rotatedY = height - rotatedY;
+          }
+          break;
+        case 180:
+          rotatedX = width - x;
+          rotatedY = height - y;
+          if (isFront() == true){ //front cam landscape
+            rotatedX = width - rotatedX;
+          }
+          break;
+        case 270:
+          rotatedX = height - y;
+          rotatedY = width - x;
+          if (isFront() == true){ //front cam portrait
+            rotatedY = height - rotatedY;
+          }
+          break;
+        default:
+          rotatedX = x;
+          rotatedY = y;
+      }
+      result["x"+i] = rotatedX;
+      result["y"+i] = rotatedY;
+    }
+  }
+
+  const isFront = () => {
+    if (cameraID == "") {
+      return false;
+    }
+    if (cameraID.toUpperCase().indexOf("BACK") != -1) { //is back cam
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  const updateViewBox = (width:number, height:number, deviceOrientation?:string) => {
+    let box:string = "0 0 "+width+" "+height;
+    if (deviceOrientation && deviceOrientation == "portrait") {
+      box = "0 0 "+height+" "+width;
+    }
+    setViewBox(box);
+  }
+
   if (initialized == false) {
-    return <p>Initializing</p>
+    return <div style={{zIndex: 999}}><p>Initializing</p></div>
   }
 
   return (
@@ -154,12 +207,12 @@ const Scanner = (props:RouteComponentProps) => {
             </select>
             <button className="close-button controls" onClick={onClosed}>Close</button>
             <svg
-              viewBox={"0 0 "+frameWidth+" "+frameHeight}
+              viewBox={viewBox}
               className="overlay"
               xmlns="<http://www.w3.org/2000/svg>"
             >
-              {barcodeResults.map(tr => (
-                    <polygon xmlns="<http://www.w3.org/2000/svg>"
+              {barcodeResults.map((tr,idx) => (
+                    <polygon key={"poly-"+idx} xmlns="<http://www.w3.org/2000/svg>"
                     points={getPointsData(tr)}
                     className="barcode-polygon"
                     />
