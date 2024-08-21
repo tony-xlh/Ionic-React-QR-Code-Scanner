4a6c2f963f7f3db6a95e10bd0513107612fac48e
xulihang <xulihanghai@163.com>
2024-08-21 18:08:20 +0800

    update ion background color when needed

--- src/pages/Scanner.tsx
+++ src/pages/Scanner.tsx
@@ -13,6 +13,15 @@ const Scanner = (props:RouteComponentProps)  => {
   const scanned = useRef(false);
   const [torchOn,setTorchOn] = useState(false);
   const [barcodeResults,setBarcodeResults] = useState([] as TextResult[]);
+  const ionBackground = useRef("");
+  
+  useEffect(() => {
+    ionBackground.current = document.documentElement.style.getPropertyValue('--ion-background-color');
+    return () => {
+      document.documentElement.style.setProperty('--ion-background-color', ionBackground.current);
+    }
+  }, []);
+
   useEffect(() => {
     const state = props.location.state as { continuousScan?: boolean };
     if (state) {
@@ -31,11 +40,13 @@ const Scanner = (props:RouteComponentProps)  => {
   }
   
   const goBack = () => {
+    document.documentElement.style.setProperty('--ion-background-color', ionBackground.current);
     props.history.goBack();
   }
 
   const onPlayed = (result:{orientation:"LANDSCAPE"|"PORTRAIT",resolution:string}) => {
     console.log(result);
+    document.documentElement.style.setProperty('--ion-background-color', 'transparent');
     let width = parseInt(result.resolution.split("x")[0]);
     let height = parseInt(result.resolution.split("x")[1]);
     let box:string;
@@ -53,6 +64,7 @@ const Scanner = (props:RouteComponentProps)  => {
       setBarcodeResults(results);
     }else{
       if (results.length>0 && scanned.current === false) {
+        document.documentElement.style.setProperty('--ion-background-color', ionBackground.current);
         scanned.current = true;
         props.history.replace({ state: {results:results} });
         props.history.goBack();
