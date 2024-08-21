6a09e07879ba6173656eada0338325ad59dffa3b
xulihang <xulihanghai@163.com>
2024-08-21 15:52:56 +0800

    reinit using ionic 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import {
-  IonApp,
-  IonRouterOutlet,
-  setupIonicReact
-} from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-
+import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,26 +19,35 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css';
+
 /* Theme variables */
 import './theme/variables.css';
-import Home from './pages/Home';
-import Scanner from './pages/Scanner';
-import { Redirect, Route } from 'react-router';
 
 setupIonicReact();
 
-const App: React.FC = () => {
-  return (
-    <IonApp style={{zIndex:999}}>
+const App: React.FC = () => (
+  <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route path="/scanner" component={Scanner} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
-  );
-}
+);
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,80 +1,25 @@
-import { IonButton, IonCheckbox, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonTitle, IonToolbar, useIonToast } from "@ionic/react";
-import { TextResult } from "capacitor-plugin-dynamsoft-barcode-reader";
-import { useEffect, useState } from "react";
-import { RouteComponentProps } from "react-router";
-import copy from 'copy-to-clipboard';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
 
-const Home = (props:RouteComponentProps) => {
-  const [continuousScan, setContinuousScan] = useState(false);
-  const [present, dismiss] = useIonToast();
-  const [QRcodeOnly, setQRcodeOnly] = useState(true);
-  const [barcodeResults, setBarcodeResults] = useState([] as TextResult[]);
-
-  useEffect(() => {
-    const state = props.location.state as { results?: TextResult[] };
-    console.log(state);
-    if (state) {
-      if (state.results) {
-        setBarcodeResults(state.results);
-        props.history.replace({ state: {} });
-      }
-    }
-  }, [props.location.state]);
-  
-  const handleOption = (e: any) => {
-    let value = e.detail.value;
-    let checked = e.detail.checked;
-    if (value == "Continuous Scan") {
-      setContinuousScan(checked)
-    } else if (value == "Scan QR Code Only") {
-      setQRcodeOnly(checked);
-    }
-  }
-
-  const startScan = () => {
-    props.history.push("scanner",{continuousScan:continuousScan,qrcodeOnly:QRcodeOnly,active:true})
-  }
-
-  const copyBarcode = (text:string) => {
-    if (copy(text)){
-      present("copied",500);
-    }
-  }
-
+const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>QR Code Scanner</IonTitle>
+          <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonButton expand="full" onClick={startScan}>Start Scanning</IonButton>
-        <IonList>
-          <IonItem>
-            <IonLabel>Continuous Scan</IonLabel>
-            <IonCheckbox slot="end" value="Continuous Scan" checked={continuousScan} onIonChange={(e) => handleOption(e)}/>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Scan QR Code Only</IonLabel>
-            <IonCheckbox slot="end" value="Scan QR Code Only" checked={QRcodeOnly} onIonChange={(e) => handleOption(e)}/>
-          </IonItem>
-          {(barcodeResults.length>0) &&
-            <IonListHeader>
-              <IonLabel>Results:</IonLabel>
-            </IonListHeader>
-          }
-          {barcodeResults.map((tr,idx) => (
-            <IonItem key={idx}>
-              <IonLabel>{(idx+1) + ". " + tr.barcodeFormat + ": " + tr.barcodeText}</IonLabel>
-              <IonLabel style={{color:"green"}} slot="end" onClick={() =>{copyBarcode(tr.barcodeText)}}>copy</IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
+};
 
-}
-  
 export default Home;
